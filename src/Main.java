c551f5ed68723ec1cbc33b1b14d907dc27fe2ec5
Cleython-Enginner <cleythonsantos@gmail.com>
2021-08-25 22:02:45 -0300

    adiciona novos cursos para os Bootcamps
    Se aceito esse commit adiciona novos cursos e usuarios
    
    closes #2

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,35 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
-        curso1.setTitulo("curso java");
-        curso1.setDescricao("descrição curso java");
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso java");
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso2.setTitulo("curso js");
-        curso2.setDescricao("descrição curso js");
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição curso JavaScript");
         curso2.setCargaHoraria(4);
 
+        Curso curso3 = new Curso();
+        curso3.setTitulo("Curso React");
+        curso3.setDescricao("Descrição curso React");
+        curso3.setCargaHoraria(6);
+
+        Curso curso4 = new Curso();
+        curso4.setTitulo("Curso Spring");
+        curso4.setDescricao("Descrição curso Spring");
+        curso4.setCargaHoraria(10);
+
+        Curso curso5 = new Curso();
+        curso5.setTitulo("Curso Banco de Dados");
+        curso5.setDescricao("Descrição curso BD avançado");
+        curso5.setCargaHoraria(8);
+
         Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("mentoria de java");
-        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setTitulo("Mentoria de Java Avançado");
+        mentoria.setDescricao("Descrição mentoria java");
         mentoria.setData(LocalDate.now());
+
 
         /*System.out.println(curso1);
         System.out.println(curso2);
@@ -30,7 +46,7 @@ public class Main {
         bootcamp.setNome("Bootcamp Java Developer");
         bootcamp.setDescricao("Descrição Bootcamp Java Developer");
         bootcamp.getConteudos().add(curso1);
-        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(curso4);
         bootcamp.getConteudos().add(mentoria);
 
         Dev devCamila = new Dev();
@@ -51,12 +67,35 @@ public class Main {
         devJoao.inscreverBootcamp(bootcamp);
         System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
         devJoao.progredir();
-        devJoao.progredir();
-        devJoao.progredir();
         System.out.println("-");
         System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
         System.out.println("Conteúdos Concluidos João:" + devJoao.getConteudosConcluidos());
         System.out.println("XP:" + devJoao.calcularTotalXp());
+
+        Dev devClara = new Dev();
+        devClara.setNome("Clara");
+        devClara.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Clara:" + devClara.getConteudosInscritos());
+        devClara.progredir();
+        devClara.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Clara:" + devClara.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Clara:" + devClara.getConteudosConcluidos());
+        System.out.println("XP:" + devClara.calcularTotalXp());
+
+        Dev devCleython = new Dev();
+        devCleython.setNome("Cleython");
+        devCleython.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Cleython:" + devCleython.getConteudosInscritos());
+        devCleython.progredir();
+        devCleython.progredir();
+        devCleython.progredir();
+        devCleython.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Cleython:" + devCleython.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Cleython:" + devCleython.getConteudosConcluidos());
+        System.out.println("XP:" + devCleython.calcularTotalXp());
+
 
     }
 
